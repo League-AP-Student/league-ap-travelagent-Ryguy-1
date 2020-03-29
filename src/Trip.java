ab73425e988fb3c2ab2b0350e55f55ba102a83ea
Ryguy-1 <Ryguy-1@users.noreply.github.com>
2020-03-29 15:56:37 -0700

    done

--- src/Trip.java
+++ src/Trip.java
@@ -1,15 +1,41 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Trip {
 	private List<Flight> flights;
 
 	public int getDuration() {
-		int duration = 0;
-		return duration;
-	}
+
+		if(flights.size() == 0) {
+			return 0;
+		}
+		
+		Time t1 = flights.get(0).getDeparturetime();
+		Time t2 = flights.get(flights.size()-1).getArrivalTime();
+		return t1.minutesUntil(t2);
+		}
 
 	public int getShortestLayover() {
-		int shortest = -1;
-		return shortest;
+		if(flights.size()<2) {
+			return -1;
+		}
+		
+		
+		ArrayList<Integer> layovers = new ArrayList<Integer>(flights.size()-1);
+		
+		for (int i = 0; i < flights.size()-1; i++) {
+			Time t1 = flights.get(i).getArrivalTime();
+			Time t2 = flights.get(i+1).getDeparturetime();
+			layovers.add(t1.minutesUntil(t2));
+		}
+		int temp = layovers.get(0);
+		for (int i = 0; i < layovers.size(); i++) {
+			
+			if(temp>layovers.get(i)) {
+				temp=layovers.get(i);
+			}
+		}
+		return temp;
+		
 	}
 }
